6feac41944ac94317c6c3099c19dc1b104a0f085
Dariagan <seomasini@gmail.com>
2025-10-12 14:02:08 +0200

    base preparada

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ https://github.com/edbeeching/godot_rl_agents
 https://github.com/edbeeching/godot_rl_agents_examples
 
 source venv/bin/activate
-//
 
+https://www.youtube.com/watch?v=f8arMv_rtUU
+
+
+https://www.oreilly.com/library/view/godot-engine-game/9781788831505/7fb4274c-5ae3-4833-b394-6d5115cac0a8.xhtml
---
+++ godo/scripts/ai_controller_3d.gd
@@ -0,0 +1,45 @@
+extends AIController3D
+
+var move: Vector2 = Vector2.ZERO
+@onready var cube = $".."
+@onready var target = $"../../Target"
+
+func get_obs() -> Dictionary:
+	var obs := [
+		cube.position.x,
+		cube.position.z,
+		target.position.x,
+		target.position.z
+		
+	]
+	
+	return {"obs": obs}
+
+func get_reward() -> float:    
+	return reward
+	
+func get_action_space() -> Dictionary:
+	return {
+		"move": {
+			"size": 2,
+			"action_type": "continuous"
+		},
+	}
+	
+func set_action(action) -> void:    
+	move.x = action["move"][0]
+	move.y = action["move"][1]
+	
+# -----------------------------------------------------------------------------#
+
+#-- Methods that can be overridden if needed --#
+
+#func get_obs_space() -> Dictionary:
+# May need overriding if the obs space is complex
+#	var obs = get_obs()
+#	return {
+#		"obs": {
+#			"size": [len(obs["obs"])],
+#			"space": "box"
+#		},
+#	}
