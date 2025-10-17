2546d9891fdbb267ef627046770421b5fac81ef8
Dariagan <seomasini@gmail.com>
2025-10-17 19:03:51 +0200

    sensor

--- godo/scripts/monke_raycast.gd
+++ godo/scripts/monke_raycast.gd
@@ -7,8 +7,6 @@ func calculate_raycasts() -> Array:
         ray.force_raycast_update()
         
         var dist: float = _get_raycast_distance(ray)
-        if dist == 0:
-            dist = 999999999999999.0
         
         result.append(dist)
 
