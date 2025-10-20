21731d9a4cb4e3a1d425c51cc63af34fe57d70f2
Dariagan <seomasini@gmail.com>
2025-10-20 22:21:11 +0200

    e

--- new.sh
+++ new.sh
@@ -3,4 +3,4 @@ source /home/stefan/Documents/robotica/venv/bin/activate
 
 # Generate experiment name based on the current date and time
 EXPERIMENT_NAME="new_$(date +'%B%d-%H:%M')"
-python stable_baselines3_example.py --env_path=/home/stefan/Documents/robotica/godo.x86_64 --experiment_name=$EXPERIMENT_NAME --timesteps=200_000_000 --save_checkpoint_frequency=200_000 --speedup=10 --n_parallel=14 
+python stable_baselines3_example.py --env_path=/home/stefan/Documents/robotica/godo.x86_64 --experiment_name=$EXPERIMENT_NAME --timesteps=200_000_000 --save_checkpoint_frequency=200_000 --speedup=10 --n_parallel=7 --n_climbers=50 
--- resume.sh
+++ resume.sh
@@ -11,4 +11,4 @@ RESUME_PATH=$1
 # Generate experiment name based on the current date and time
 EXPERIMENT_NAME="$(date +'%B%d-%H:%M')"
 
-python stable_baselines3_example.py --env_path=/home/stefan/Documents/robotica/godo.x86_64 --experiment_name="$EXPERIMENT_NAME" --timesteps=100_000_000_000 --save_checkpoint_frequency=200_000 --speedup=10 --n_parallel=14 --save_model_path=nase.zip --resume_model_path="$RESUME_PATH"
+python stable_baselines3_example.py --env_path=/home/stefan/Documents/robotica/godo.x86_64 --experiment_name="$EXPERIMENT_NAME" --timesteps=100_000_000_000 --save_checkpoint_frequency=200_000 --speedup=10 --n_parallel=8 --save_model_path=nase.zip --resume_model_path="$RESUME_PATH"
