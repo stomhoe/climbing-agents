52c229d145cb8bfe9af0f27666991c58aad22937
Dariagan <seomasini@gmail.com>
2025-11-06 15:15:55 +0100

    scripts laptop

--- climbingpvp.sh
+++ climbingpvp.sh
@@ -1,24 +1,54 @@
 #!/usr/bin/env bash
 source "venv/bin/activate"
 
-RESUME_PATH=$(find "$(pwd)/logs" -name "*.zip" -print0 | xargs -0 ls -lt | head -n 1 | awk '{print $9}')
+NEW_RUN=false
+VIZ=""
 
-# Generate experiment name based on the current date and time
-EXPERIMENT_NAME="pvpclimb-reslat$(date +'%B%d-%H:%M')"
+# Parse arguments
+for arg in "$@"; do
+    if [ "$arg" = "n" ]; then
+        NEW_RUN=true
+    fi
+    if [ "$arg" = "v" ]; then
+        VIZ="--viz"
+    fi
+done
 
-python stable_baselines3_example.py \
-    --n_climbers=5 \
-    --random=1.0 \
-    --infection_ratio=0.0 \
-    --round_duration=300 \
-    --n_arenas=10 \
-    --pvp=0 \
-    --collision_enabled=false \
-    --n_parallel=6 \
-    --onnx_export_path=model.onnx \
-    --save_checkpoint_frequency=200_000 \
-    --speedup=10 \
-    --env_path=godo.x86_64 \
-    --experiment_name="$EXPERIMENT_NAME" \
-    --timesteps=100_000_000_000 \
-    --resume_model_path="$RESUME_PATH"                                                                                                                                                                                                                                      
+if [ "$NEW_RUN" = false ]; then
+    RESUME_PATH=$(find "$(pwd)/logs" -name "*.zip" -print0 | xargs -0 ls -lt | head -n 1 | awk '{print $9}')
+    RESUME_ARG="--resume_model_path=$RESUME_PATH"
+    RANDOM="--random=1"
+    RAND_INCR=""
+    RAND_CAP=""
+else
+    RESUME_ARG=""
+    RANDOM="--random=0"
+    RAND_INCR="--rand_incr=0.01"
+    RAND_CAP="--rand_cap=1"
+fi
+EXPERIMENT_NAME="pvp$(date +'%B%d-%H:%M:%S')"
+
+while true; do
+    python stable_baselines3_example.py \
+        --n_climbers=3 \
+        $RANDOM \
+        $RAND_INCR \
+        $RAND_CAP \
+        --infection_ratio=0.0 \
+        --round_duration=300 \
+        --n_arenas=20 \
+        --pvp=0 \
+        --collision_enabled=true \
+        $VIZ \
+        --n_parallel=$([ "$VIZ" = "--viz" ] && echo 1 || echo 7) \
+        --onnx_export_path=model.onnx \
+        $([ "$VIZ" != "--viz" ] && echo "--save_checkpoint_frequency=50_000") \
+        --speedup=$([ "$VIZ" = "--viz" ] && echo 1 || echo 10) \
+        --env_path=godo.x86_64 \
+        --experiment_name="$EXPERIMENT_NAME" \
+        --timesteps=100_000_000_000 \
+        $RESUME_ARG
+
+    RESUME_PATH=$(find "$(pwd)/logs" -name "*.zip" -print0 | xargs -0 ls -lt | head -n 1 | awk '{print $9}')
+    RESUME_ARG="--resume_model_path=$RESUME_PATH"
+done
--- infection.sh
+++ infection.sh
@@ -3,7 +3,6 @@ source "venv/bin/activate"
 
 NEW_RUN=false
 VIZ=""
-COLLISION_ENABLED="--collision_enabled=false"
 
 # Parse arguments
 for arg in "$@"; do
@@ -13,36 +12,33 @@ for arg in "$@"; do
     if [ "$arg" = "v" ]; then
         VIZ="--viz"
     fi
-    if [ "$arg" = "c" ]; then
-        COLLISION_ENABLED="--collision_enabled=true"
-    fi
 done
 
 if [ "$NEW_RUN" = false ]; then
     RESUME_PATH=$(find "$(pwd)/logs" -name "*.zip" -print0 | xargs -0 ls -lt | head -n 1 | awk '{print $9}')
     RESUME_ARG="--resume_model_path=$RESUME_PATH"
-    RANDOM="--random\\=1"
+    RANDOM="--random=1"
     RAND_INCR=""
     RAND_CAP=""
 else
     RESUME_ARG=""
-    RANDOM="--random\\=0"
+    RANDOM="--random=0"
     RAND_INCR="--rand_incr=0.01"
     RAND_CAP="--rand_cap=1"
 fi
-EXPERIMENT_NAME="nocol$(date +'%B%d-%H:%M:%S')"
+EXPERIMENT_NAME="infection$(date +'%B%d-%H:%M:%S')"
 
 while true; do
     python stable_baselines3_example.py \
-        --n_climbers=$([ "$VIZ" = "--viz" ] && echo 10 || echo 70) \
+        --n_climbers=5 \
         $RANDOM \
         $RAND_INCR \
         $RAND_CAP \
-        --infection_ratio=0.0 \
-        --round_duration=300 \
-        --n_arenas=1 \
+        --infection_ratio=1.0 \
+        --round_duration=180 \
+        --n_arenas=3 \
         --pvp=0 \
-        $COLLISION_ENABLED \
+        --collision_enabled=false \
         $VIZ \
         --n_parallel=$([ "$VIZ" = "--viz" ] && echo 1 || echo 7) \
         --onnx_export_path=model.onnx \
