f1f8c22cb728c57b735540f598413ab3ba6158cb
Dariagan <seomasini@gmail.com>
2025-11-06 13:22:29 +0100

    e

--- climbingnocol.sh
+++ climbingnocol.sh
@@ -28,22 +28,28 @@ else
 fi
 EXPERIMENT_NAME="nocol$(date +'%B%d-%H:%M:%S')"
 
-python stable_baselines3_example.py \
-    --n_climbers=$([ "$VIZ" = "--viz" ] && echo 10 || echo 70) \
-    $RANDOM \
-    $RAND_INCR \
-    $RAND_CAP \
-    --infection_ratio=0.0 \
-    --round_duration=300 \
-    --n_arenas=1 \
-    --pvp=0 \
-    --collision_enabled=false \
-    $VIZ \
-    --n_parallel=$([ "$VIZ" = "--viz" ] && echo 1 || echo 7) \
-    --onnx_export_path=model.onnx \
-    --save_checkpoint_frequency=200_000 \
-    --speedup=$([ "$VIZ" = "--viz" ] && echo 1 || echo 10) \
-    --env_path=godo.x86_64 \
-    --experiment_name="$EXPERIMENT_NAME" \
-    --timesteps=100_000_000_000 \
-    $RESUME_ARG
+while true; do
+    python stable_baselines3_example.py \
+        --n_climbers=$([ "$VIZ" = "--viz" ] && echo 10 || echo 70) \
+        $RANDOM \
+        $RAND_INCR \
+        $RAND_CAP \
+        --infection_ratio=0.0 \
+        --round_duration=300 \
+        --n_arenas=1 \
+        --pvp=0 \
+        --collision_enabled=false \
+        $VIZ \
+        --n_parallel=$([ "$VIZ" = "--viz" ] && echo 1 || echo 7) \
+        --onnx_export_path=model.onnx \
+        $([ "$VIZ" != "--viz" ] && echo "--save_checkpoint_frequency=50_000") \
+        --speedup=$([ "$VIZ" = "--viz" ] && echo 1 || echo 10) \
+        --env_path=godo.x86_64 \
+        --experiment_name="$EXPERIMENT_NAME" \
+        --timesteps=100_000_000_000 \
+        $RESUME_ARG
+
+    RESUME_PATH=$(find "$(pwd)/logs" -name "*.zip" -print0 | xargs -0 ls -lt | head -n 1 | awk '{print $9}')
+    RESUME_ARG="--resume_model_path=$RESUME_PATH"
+done
+
